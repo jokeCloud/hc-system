70390452dcbbad258f6e8d7d7ed4c7e3053af596
jokecloud <medalhaemerson@gmail.com>
2022-10-05 17:21:04 -0300

    cadastro de clientes completo

--- health_care_app/admin.py
+++ health_care_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from health_care_app.models import Patients
+
+
+class PatientsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'phone', 'email', 'gender', 'created_at']
+    search_fields = ['id', 'name', 'phone', 'email', 'gender']
+    list_per_page = 8
+
+
+admin.site.register(Patients, PatientsAdmin)
--- health_care_app/views.py
+++ health_care_app/views.py
@@ -1,5 +1,9 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from health_care_app.models import Patients
 
 
 def frontend(request):
@@ -13,4 +17,17 @@ def backend(request):
 
 @login_required(login_url='login')
 def add_patient(request):
-    return render(request, 'add.html')
+    if request.method == 'POST':
+        if request.POST.get('name') and request.POST.get('phone') and request.POST.get('email') and request.POST.get('age') and request.POST.get('gender') or request.POST.get('note'):  # noqa
+            patient = Patients()
+            patient.name = request.POST.get('name')
+            patient.phone = request.POST.get('phone')
+            patient.email = request.POST.get('email')
+            patient.age = request.POST.get('age')
+            patient.gender = request.POST.get('gender')
+            patient.note = request.POST.get('note')
+            patient.save()
+            messages.success(request, 'Patient added sucessfully!')
+            return HttpResponseRedirect('/backend')
+    else:
+        return render(request, 'add.html')
