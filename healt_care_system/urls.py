e676c909b130bf2dc8e933638e64b0ccde867443
jokecloud <medalhaemerson@gmail.com>
2022-10-06 10:14:08 -0300

    list patients

--- healt_care_system/urls.py
+++ healt_care_system/urls.py
@@ -3,13 +3,18 @@ from django.urls import include, path
 from health_care_app import views
 
 urlpatterns = [
+    # path to access the admin page
     path('admin/', admin.site.urls),
+    # path to render homepage
     path('', views.frontend, name='frontend'),
+    # path to login/logout
     path('login/', include('django.contrib.auth.urls')),
 
     # ================ #
     # BACKEND SECTION  #
     # ================ #
+    # path to access the backend page
     path('backend/', views.backend, name='backend'),
+    # path to add patient
     path('add_patient', views.add_patient, name='add_patient'),
 ]
--- health_care_app/views.py
+++ health_care_app/views.py
@@ -1,20 +1,38 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from health_care_app.models import Patients
 
 
+# Function to render the frontend
 def frontend(request):
     return render(request, 'frontend.html')
 
 
+# Function to render the backend
 @login_required(login_url='login')
 def backend(request):
-    return render(request, 'backend.html')
+    if 'q' in request.GET:
+        q = request.GET['q']
+        all_patient_list = Patients.objects.filter(
+            Q(name__icontains=q) | Q(phone=q) | Q(
+                email=q) | Q(age=q) | Q(gender=q) | Q(note=q)
+        ).order_by('-created_at')
+    else:
+        all_patient_list = Patients.objects.all().order_by('-created_at')
+
+    paginator = Paginator(all_patient_list, 10)
+    page = request.GET.get('page')
+    all_patient = paginator.get_page(page)
+
+    return render(request, 'backend.html', {'patients': all_patient})
 
 
+# Function to insert patient
 @login_required(login_url='login')
 def add_patient(request):
     if request.method == 'POST':
